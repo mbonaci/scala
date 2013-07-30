e1728555ab495d04017525e066f8ba617734ee39
mbonaci <mbonaci@gmail.com>
2013-07-30 18:31:43 +0200

    Chapter 9 (v2)

---
+++ scala2e-book/src/scala2e/chapter9/PrintWriterHelper.scala
@@ -0,0 +1,21 @@
+package scala2e.chapter9
+
+import java.io.File
+
+object PrintWriterHelper {
+
+  def main(args: Array[String]): Unit = {
+    withPrintWriter(
+      new File("date.txt"), 
+      writer => writer.println(new java.util.Date))
+  }
+
+  def withPrintWriter(file: File, op: java.io.PrintWriter => Unit) {
+    val writer = new java.io.PrintWriter(file)
+    try {
+      op(writer)
+    } finally {
+      writer.close()
+    }
+  }
+}
