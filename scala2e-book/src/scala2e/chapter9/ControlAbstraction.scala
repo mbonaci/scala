d5368ca62003c8cc39eff8ff1174b34752fa405b
mbonaci <mbonaci@gmail.com>
2013-07-30 18:14:38 +0200

    Chapter 9 (v1)

---
+++ scala2e-book/src/scala2e/chapter9/ControlAbstraction.scala
@@ -0,0 +1,85 @@
+package scala2e.chapter9
+
+object ControlAbstraction {
+
+  def main(args: Array[String]): Unit = {
+//    val files = FileMatcher.filesEnding(".scala")
+//    for(file <- files) println("Ends: " + file.getPath)
+//    
+//    val fref = FileMatcherRefactored.filesContaining("ner")
+//    for(file <- fref) println("Contains: " + file.getPath)
+//    
+//    val fref2 = FileMatcherRefactored2.filesRegex(".*.sc*")
+//    for(file <- fref) println("Regex: " + file.getPath)
+    
+    val lst = List(0, 2, 4, 992, 12)
+    println(ClientCode.containsOddNum(lst))
+  }
+
+  private def filesHere = (new java.io.File("src")).listFiles
+  
+  object FileMatcher {
+    
+    
+	def filesEnding(query: String) =
+	  for (
+	    file <- filesHere;
+	    if file.getName.endsWith(query)
+	  ) yield file
+	  
+	def filesContaining(query: String) =
+	  for(
+	    file <- filesHere;
+	    if file.getName.contains(query)
+	  ) yield file
+	
+	def filesRegex(query: String) =
+	  for(
+	    file <- filesHere;
+	    if file.getName.matches(query)
+	  ) yield file
+
+  }
+  
+    
+  // pass a function value that calls the method
+  object FileMatcherRefactored {
+    def filesMatching(query: String, matcher: (String, String) => Boolean) = {
+      for(
+        file <- filesHere;
+        if matcher(file.getName, query)
+      ) yield file
+    }
+    
+    def filesEnding(query: String) =
+      filesMatching(query, _.endsWith(_))
+      
+    def filesContaining(query: String) = 
+      filesMatching(query, _.contains(_))
+      
+    def filesRegex(query: String) =
+      filesMatching(query, _.matches(_))
+  }
+  
+  object FileMatcherRefactored2 {
+    def filesMatching(matcher: String => Boolean) = {
+      for(
+        file <- filesHere;
+        if matcher(file.getName)
+      ) yield file
+    }
+    
+    def filesEnding(query: String) =
+      filesMatching(_.endsWith(query))
+      
+    def filesContaining(query: String) = 
+      filesMatching(_.contains(query))
+      
+    def filesRegex(query: String) =
+      filesMatching(_.matches(query))
+  }
+  
+  object ClientCode {
+    def containsOddNum(nums: List[Int]) = nums.exists(_ % 2 == 1)
+  }
+}
