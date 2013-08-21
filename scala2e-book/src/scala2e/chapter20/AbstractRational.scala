56b5c0292daa97371f4971e505d48f4a064423e1
mbonaci <mbonaci@gmail.com>
2013-08-21 19:38:22 +0200

    Chapter20 (v7) Initializing abstract vals (source)

--- scala2e-book/src/scala2e/chapter20/AbstractRational.scala
+++ scala2e-book/src/scala2e/chapter20/AbstractRational.scala
@@ -2,15 +2,40 @@ package scala2e.chapter20
 
 object AbstractRational {
 
+  // instead of class with two class parameters
   trait RationalTrait {
     val numerArg: Int
     val denomArg: Int
   }
   
   def main(args: Array[String]): Unit = {
-    new RationalTrait {
-      val numerArg = 1
-      val denomArg = 2
+    // example implementation of two abstract vals
+    // yields an instance of an anonymous class
+    // which mixes in the trait and is defined by the body
+//    new RationalTrait {
+//      val numerArg = 1
+//      val denomArg = 2
+//    }
+    
+    val x = 2
+    val fun = new ProblematicRationalTrait {
+      val numerArg = 1 * x
+      val denomArg = 2 * x
     }
+  }
+  
+  trait ProblematicRationalTrait {
+    val numerArg: Int
+    val denomArg: Int
+    require(denomArg != 0)  // throws "requirement failed" exception
+    private val g = gcd(numerArg, denomArg)
+    val numer = numerArg / g
+    val denom = denomArg / g
+    
+    private def gcd(a: Int, b: Int): Int =
+      if (b == 0) a
+      else gcd(b, a % b)
+    
+    override def toString = numer + "/" + denom
   }
 }
