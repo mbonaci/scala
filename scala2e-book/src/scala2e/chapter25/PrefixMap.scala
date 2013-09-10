1cb80bdcea43dcf11be9ff337e894917f6dec1c4
mbonaci <mbonaci@gmail.com>
2013-09-10 22:50:29 +0200

    Chapter25 (v8) Integrating new collections: Integrating new sets and maps (source v1)

---
+++ scala2e-book/src/scala2e/chapter25/PrefixMap.scala
@@ -0,0 +1,46 @@
+package scala2e.chapter25
+
+object PrefixMap {
+
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+  // PrefixMap implementation (Patricia trie based)
+  import collection._
+  class PrefixMap[T] extends mutable.Map[String, T] with mutable.MapLike[String, T, PrefixMap[T]] {
+    var suffixes: immutable.Map[Char, PrefixMap[T]] = Map.empty
+    var value: Option[T] = None
+  
+    def get(s: String): Option[T] =
+      if (s.isEmpty) value
+      else suffixes get (s(0)) flatMap (_.get(s substring 1))
+  
+    def withPrefix(s: String): PrefixMap[T] =
+      if (s.isEmpty) this
+      else {
+        val leading = s(0)
+        suffixes get leading match {
+          case None => suffixes = suffixes + (leading -> empty)
+          case _ =>
+        }
+        suffixes(leading) withPrefix (s substring 1)
+      }
+  
+    override def update(s: String, elem: T) =
+      withPrefix(s).value = Some(elem)
+    override def remove(s: String): Option[T] =
+      if (s.isEmpty) { val prev = value; value = None; prev}
+      else suffixes get (s(0)) flatMap (_.remove(s substring 1))
+      
+    def iterator: Iterator[(String, T)] =
+      (for (v <- value.iterator) yield ("", v)) ++
+      (for ((chr, m) <- suffixes.iterator;
+          (s, v) <- m.iterator) yield (chr +: s, v))
+  
+    def += (kv: (String, T)): this.type = { update(kv._1, kv._2); this }
+    def -= (s: String): this.type = { remove(s); this }
+    override def empty = new PrefixMap[T]  
+  }
+  
+}
