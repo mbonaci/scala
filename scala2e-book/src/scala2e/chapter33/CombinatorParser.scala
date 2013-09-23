4ecdbc0201356be538ef99cb50464ff94c9e59f7
mbonaci <mbonaci@gmail.com>
2013-09-23 21:11:11 +0200

    Chapter33 (v7) JSON parser (source)

--- scala2e-book/src/scala2e/chapter33/CombinatorParser.scala
+++ scala2e-book/src/scala2e/chapter33/CombinatorParser.scala
@@ -7,11 +7,29 @@ class Arith extends JavaTokenParsers {
   def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
   def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
 }
-
+/*
 object ParseExpr extends Arith {
   def main(args: Array[String]) {
     val arg = "2 * (3 + 7))"
     println("input: " + arg)
     println(parseAll(expr, arg))
+  }
+}
+*/
+import scala.util.parsing.combinator._
+class JSON extends JavaTokenParsers {
+  def value:   Parser[Any] = obj | arr | stringLiteral | 
+                             floatingPointNumber | "null" | "true" | "false"
+  def obj:     Parser[Any] = "{"~repsep(member, ",")~"}"
+  def arr:     Parser[Any] = "["~repsep(value, ",")~"]"
+  def member:  Parser[Any] = stringLiteral~":"~value
+}
+
+import java.io.FileReader
+object ParseJSON extends JSON {
+  def main(args: Array[String]) {
+    val reader = new FileReader(args(0))
+    // parseAll is overloaded: takes sequence or input reader as a second argument
+    println(parseAll(value, reader))
   }
 }
