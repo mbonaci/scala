a3a559ac687e9339a058eccf66319937ca61ff74
mbonaci <mbonaci@gmail.com>
2013-09-25 21:02:55 +0200

    Chapter33 (v10) Parser output (final source)

--- scala2e-book/src/scala2e/chapter33/CombinatorParser.scala
+++ scala2e-book/src/scala2e/chapter33/CombinatorParser.scala
@@ -19,18 +19,33 @@ object ParseExpr extends Arith {
 import scala.util.parsing.combinator._
 class JSON extends JavaTokenParsers {
   def value:   Parser[Any] = obj | arr | stringLiteral | 
-                             floatingPointNumber | "null" | "true" | "false"
+                             floatingPointNumber | "null" | "true" | "false";
   def obj:     Parser[Any] = "{"~repsep(member, ",")~"}"
-//  def obj: Parser[Map[String, Any]] = // Can be improved
-//    "{"~repsep(member, ",")~"}" ^^ { case "{"~ms~"}" => Map() ++ ms }
-//  def obj: Parser[Map[String, Any]] =
-//    "{" ~> repsep(member, ",") <~ "}" ^^ (Map() ++ _)
-  def arr:     Parser[Any] = "["~repsep(value, ",")~"]"
-  def member:  Parser[Any] = stringLiteral~":"~value
+ 
+  def arr:     Parser[Any] = "["~repsep(value, ",")~"]";
+  def member:  Parser[Any] = stringLiteral~":"~value;
+}
+
+class JSON1 extends JavaTokenParsers {
+  def obj: Parser[Map[String, Any]] =
+    "{" ~> repsep(member, ",") <~ "}" ^^ (Map() ++ _)
+  def arr: Parser[List[Any]] =
+    "[" ~> repsep(value, ",") <~ "]"
+  def member: Parser[(String, Any)] =
+    stringLiteral~":"~value ^^ { case name~":"~value => (name, value) }
+  def value: Parser[Any] = (
+    obj
+    | arr
+    | stringLiteral
+    | floatingPointNumber ^^ (_.toDouble)
+    | "null"  ^^ (x => null)
+    | "true"  ^^ (x => true)
+    | "false" ^^ (x => false)
+  )
 }
 
 import java.io.FileReader
-object ParseJSON extends JSON {
+object ParseJSON extends JSON1 {
   def main(args: Array[String]) {
     val reader = new FileReader(args(0))
     // parseAll is overloaded: takes sequence or input reader as a second argument
